88f076616424e3ed8cd6cab2f97fc153b7c43eb1
ilyass <ilyasstube80@gmail.com>
2024-09-07 13:48:06 +0100

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },  // Default route
+  { path: '**', redirectTo: '/dashboard' }  // Wildcard route for 404
+
+
+
+
+
+
+
+];
